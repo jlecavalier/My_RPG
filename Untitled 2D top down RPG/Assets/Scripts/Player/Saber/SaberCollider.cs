e05c56ab0fd3477946492c4da024774ad7daefec
Jay LeCavalier <jay.lecavalier@colorado.edu>
2016-03-26 20:43:45 -0600

    Enemies fly back when killed

---
+++ Untitled 2D top down RPG/Assets/Scripts/Player/Saber/SaberCollider.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SaberCollider : MonoBehaviour {
+
+  public float enemyDeathSpeed;
+  
+  public void OnCollisionEnter2D(Collision2D other) {
+  	if (other.gameObject.tag == "Enemy") {
+  	  float x = other.contacts[0].point.x;
+  	  float y = other.contacts[0].point.y;
+  	  Vector2 dir = new Vector2(x, y);
+  	  dir = dir.normalized * enemyDeathSpeed;
+  	  other.rigidbody.AddForce(dir);
+  	}
+  }
+}
